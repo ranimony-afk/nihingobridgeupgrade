68ed40b824497cc6dab5bb3adad097f647349b9c
ranimony-afk <ranimony@gmail.com>
2026-07-27 16:21:20 +0530

    del

--- src/app/nihongo/jobs/page.tsx
+++
@@ -1,7 +0,0 @@
-import { BrandCmsPage } from "@/shared/components/BrandCmsPage";
-
-export const dynamic = "force-dynamic";
-
-export default function JapaneseJobsPage() {
-  return <BrandCmsPage brandSlug="nihongo" pageSlug="jobs" />;
-}
