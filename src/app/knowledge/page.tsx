2966afe2eb685f67a86de0b617c7ca6a21d106d1
ranimony-afk <ranimony@gmail.com>
2026-08-18 10:01:51 +0530

    A5 DEL

--- src/app/knowledge/page.tsx
+++
@@ -1,7 +0,0 @@
-import { KnowledgeExplorer } from "@/components/knowledge/knowledge-explorer";
-
-export const dynamic = "force-dynamic";
-
-export default function KnowledgePage() {
-  return <KnowledgeExplorer />;
-}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import LearningDashboard from "@/components/learning-dashboard";
-
-export const dynamic = "force-dynamic";
-
-export default function HomePage() {
-  return <LearningDashboard />;
-}
